f1cc7f8515b55534a99eb13051479c4cc1406c3e
Alessandro Desantis <desa.alessandro@gmail.com>
2016-11-15 19:48:29 +0100

    Document Pragma module

--- lib/pragma.rb
+++ lib/pragma.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 require 'pragma/version'
 
+# A pragmatic architecture for building JSON APIs.
+#
+# @author Alessandro Desantis
 module Pragma
-  # Your code goes here...
 end
