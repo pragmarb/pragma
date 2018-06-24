8212c70e91df2f3fe5846a0ff1ca703ac4f1f779
Alessandro Desantis <desa.alessandro@gmail.com>
2018-06-24 19:23:59 +0200

    Expose `type` property for API errors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+
+- Expose `type` property for API errors
+
 ## [2.4.0]
 
 ### Added
--- lib/pragma/decorator/error.rb
+++ lib/pragma/decorator/error.rb
@@ -3,9 +3,15 @@
 module Pragma
   module Decorator
     class Error < Pragma::Decorator::Base
+      include Pragma::Decorator::Type
+
       property :error_type
       property :error_message
       property :meta
+
+      def type
+        :error
+      end
     end
   end
 end
